2971db954423b97904a1703196ae7e99cae265a8
Erick Flores <a00836404@tec.mx>
2025-06-15 23:46:35 -0600

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Agroquim/', // muy importante: debe coincidir con el nombre del repo
+  base: '/Agroquim/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
